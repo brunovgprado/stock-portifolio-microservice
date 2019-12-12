81414f66bc3967742cc469819e676bb89b9b72ae
bruno prado <brunomcp2010@gmail.com>
2019-12-12 18:21:06 -0300

    add start script on package.json

--- purchase.urls/purchase.urls.js
+++ purchase.urls/purchase.urls.js
@@ -1,6 +1,6 @@
 const url = {
-    REGISTER: '<YOUR-API-PATH>',
-    PORTFOLIO: '<YOUR-API-PATH>',
+    REGISTER: 'https://b9nvq8jnwh.execute-api.us-east-1.amazonaws.com/default/StockPurchase',
+    PORTFOLIO: 'https://b9nvq8jnwh.execute-api.us-east-1.amazonaws.com/default/StockPurchase',
     EQUITY_INCREASE: '<YOUR-API-PATH>'
 }
 exports.url = url;
